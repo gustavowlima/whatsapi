ab9742ad1808cec43f55338f0e17aa794a94375b
Melissa Lindince <melissalindince@proton.me>
2026-08-26 17:51:43 -0300

    feat: implement rejectCall

--- server/dto/instance.go
+++ server/dto/instance.go
@@ -44,6 +44,7 @@ type UpdateInstanceRequest struct {
 	ID           string `json:"id,omitempty" param:"id" validate:"required" swaggerignore:"true"`
 	GroupsIgnore *bool  `json:"groupsIgnore,omitempty"`
 	SaveMedia    *bool  `json:"saveMedia,omitempty"`
+	RejectCall   *bool  `json:"rejectCall,omitempty"`
 	Webhook      struct {
 		Enabled *bool    `json:"enabled,omitempty"`
 		Base64  bool     `json:"base64,omitempty"`
